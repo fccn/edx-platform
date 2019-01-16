c408e7e67c48fe115449683aee847b5d06bbec78
Luis Moreno <luis.moreno@edunext.co>
2019-01-16 17:33:52 -0400

    Discovery meaning

--- lms/envs/xen.py
+++ lms/envs/xen.py
@@ -3,3 +3,5 @@ from .aws import *
 ################### CEPH
 
 AWS_S3_HOST =  ENV_TOKENS.get('AWS_S3_HOST', 's3.amazonaws.com')
+
+COURSE_DISCOVERY_MEANINGS = ENV_TOKENS.get('COURSE_DISCOVERY_MEANINGS', {})
