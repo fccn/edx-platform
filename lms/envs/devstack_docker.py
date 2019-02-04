ca7fe2f757a708cb99ad949ab95f40cb509cf630
Luis Moreno <luis.moreno@edunext.co>
2019-02-04 16:33:12 -0400

    Settings for devstack

--- lms/envs/devstack_docker.py
+++ lms/envs/devstack_docker.py
@@ -1,3 +1,9 @@
 """ Overrides for Docker-based devstack. """
 
 from .devstack import *  # pylint: disable=wildcard-import, unused-wildcard-import
+
+FEATURES.update({
+    'ENABLE_COURSEWARE_SEARCH': True,
+    'ENABLE_COURSE_DISCOVERY': True,
+    'ENABLE_DASHBOARD_SEARCH': True,
+})
