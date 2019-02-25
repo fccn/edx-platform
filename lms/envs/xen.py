12374522bcd0667d19eb4527ca6a7313538fd69e
Luis Moreno <luis.moreno@edunext.co>
2019-02-25 13:51:04 -0400

    Use django storage for ORA2

--- lms/envs/xen.py
+++ lms/envs/xen.py
@@ -5,3 +5,5 @@ from .aws import *
 AWS_S3_HOST =  ENV_TOKENS.get('AWS_S3_HOST', 's3.amazonaws.com')
 
 COURSE_DISCOVERY_MEANINGS = ENV_TOKENS.get('COURSE_DISCOVERY_MEANINGS', {})
+
+ORA2_FILEUPLOAD_BACKEND = "django"
